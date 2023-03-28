9c7ee246c94ff8bd5dfc49b38e577ed351d2473a
fosten <fosten@gmail.com>
2023-03-28 01:43:58 -0400

    Update Edit components

--- src/components/Blocks/SingleSeasonStats/Edit.jsx
+++ src/components/Blocks/SingleSeasonStats/Edit.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 const Edit = props => {
-  return <div>I'm the auto-generated Player Stats block!  There is no need to edit me.</div>;
+  return <div>I'm the auto-generated Single Seasons Stats block!  There is no need to edit me.</div>;
 };
 export default Edit;
