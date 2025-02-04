d6832be44ba62722551b7ee9bac31b2493cf46a2
Fosten <fosten@gmail.com>
2025-02-04 14:30:13 -0500

    Hardcode max statyear in CareerStats

--- packages/volto-baseball/src/components/Blocks/CareerStats/View.jsx
+++ packages/volto-baseball/src/components/Blocks/CareerStats/View.jsx
@@ -11,7 +11,7 @@ import PropTypes from 'prop-types';
 const MLBStatsAPI = require('@asbeane/mlb-stats-api');
 const mlbStats = new MLBStatsAPI();
 function generateArrayOfYears() {
-  var max = new Date().getFullYear();
+  var max = 2024;
   var min = max - 20;
   var years = [];
 
